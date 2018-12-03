545ed1f82e57fdd791d923b476d9c990c8cf63ba
Peter Howell <peter.e.howell@gmail.com>
2018-12-03 17:58:29 -0330

    added initial md5 support

--- src/hasher.rs
+++ src/hasher.rs
@@ -1,5 +1,7 @@
 use std::fmt;
 use std::fmt::{Display, Formatter};
+use std::io;
+use std::io::Read;
 use std::path::Path;
 
 use md5::{Digest, Md5};
@@ -14,7 +16,7 @@ pub enum Algorithm {
 impl Algorithm {
     pub fn from_string(alg: &str) -> Algorithm {
         match alg {
-            "md5" => Algorithm::Md5,
+            "Md5" => Algorithm::Md5,
             "sha1" => Algorithm::Sha1,
             "sha256" => Algorithm::Sha256,
             _ => panic!("That's not an algorithm"),
@@ -35,22 +37,38 @@ impl Display for Algorithm {
 }
 
 pub fn hash_file(alg: Algorithm, fp: &Path) -> String {
-    match alg {
+    let res = match alg {
         Algorithm::Md5 => md5(fp),
-        Algorithm::Sha1 => sha1(),
-        Algorithm::Sha256 => sha256(),
+        Algorithm::Sha1 => sha1(fp),
+        Algorithm::Sha256 => sha256(fp),
+    };
+
+    match res {
+        Ok(n) => n,
+        _ => "Oops".to_string(),
     }
 }
 
-fn md5(fp: &Path) -> String {
-    let hasher = Md5::default();
+fn md5(fp: &Path) -> io::Result<String> {
+    let mut hasher = Md5::default();
+    let mut reader = std::fs::File::open(fp)?;
 
-    //hasher.result_str()
-    "MD5GoesHere".to_string()
+    let mut buffer = [0u8; 1024];
+    loop {
+        let bytes_read = reader.read(&mut buffer)?;
+        hasher.input(&buffer[..bytes_read]);
+        if bytes_read == 0 {
+            break;
+        }
+    }
+
+    let res = format!("{:x}", hasher.result());
+
+    Ok(res)
 }
-fn sha1() -> String {
-    "SHA1HashGoesHere".to_string()
+fn sha1(fp: &Path) -> io::Result<String> {
+    Ok("SHA1HashGoesHere".to_string())
 }
-fn sha256() -> String {
-    "SHA256HashGoesHere".to_string()
+fn sha256(fp: &Path) -> io::Result<String> {
+    Ok("SHA256HashGoesHere".to_string())
 }
